7b49f0ed1e8f46add3c710efb147614d23fcc531
sunlisheng <sunlisheng@xiaomi.com>
2016-08-23 15:42:16 +0800

    (同步非阻塞)NIO通讯模型

---
+++ Communication/src/nio/MultiplexerTimeServer.java
@@ -0,0 +1,121 @@
+package nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by leo on 16-8-23.
+ */
+public class MultiplexerTimeServer implements Runnable {
+
+    private Selector selector;
+
+    private ServerSocketChannel serverSocketChannel;
+
+    private volatile boolean stop;
+
+    public MultiplexerTimeServer(int port) {
+        try {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            System.out.println("The time server is start in port : " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        while(!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+                    iterator.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         *多路复用器关闭后,所有注册在上面的Channel和Pipe等资源都
+         *会被自动去注册并关闭,所以不需要重复释放资源
+         */
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            //处理新接入的请求信息
+            if (key.isAcceptable()) {
+                //Accept the new connection
+                ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                //Add the new connection to the selector
+                sc.register(selector, SelectionKey.OP_READ);
+            }
+
+            if (key.isReadable()) {
+               //Read the data
+                SocketChannel sc = (SocketChannel)key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("The time server receive order :" + body);
+                    String currentTime = "QUERY TIME ORDER"
+                            .equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString()
+                            : "BAD ORDER";
+                    doWrite(sc, currentTime);
+                }
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel socketChannel, String response) throws IOException {
+        if(response != null && response.trim().length() > 0) {
+            byte[] bytes = response.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            socketChannel.write(writeBuffer);
+        }
+    }
+}
---
+++ Communication/src/nio/TimerServer.java
@@ -0,0 +1,22 @@
+package nio;
+
+import java.io.IOException;
+
+/**
+ * Created by leo on 16-8-23.
+ */
+public class TimerServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
