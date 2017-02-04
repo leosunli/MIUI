a9cfb8c084440dbdd5e097603fdfa475c94166bb
sunlisheng <sunlisheng@xiaomi.com>
2017-02-04 10:05:32 +0800

    netty heart beat

---
+++ NettyDemo/src/main/java/heartbeat/HeartBeatServerHandler.java
@@ -0,0 +1,43 @@
+package heartbeat;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+
+/**
+ * Created by leo on 17-2-4.
+ */
+public class HeartBeatServerHandler extends ChannelInboundHandlerAdapter {
+
+    private int lossConnectTime = 0;
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            if (event.state() == IdleState.READER_IDLE) {
+                lossConnectTime++;
+                System.out.println("5秒没有收到客户端的信息了");
+                if (lossConnectTime > 2) {
+                    System.out.println("关闭这个不活跃的channel");
+                    ctx.channel().close();
+                }
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("server channelRead...");
+        System.out.println(ctx.channel().remoteAddress() + "->server : " + msg.toString());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+       cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ NettyDemo/src/main/java/heartbeat/HeartBeatsClient.java
@@ -0,0 +1,36 @@
+package heartbeat;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by leo on 17-2-4.
+ */
+public class HeartBeatsClient {
+
+    public void connect(int port, String host) throws Exception {
+        //Configure the client
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                        }
+                    });
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
